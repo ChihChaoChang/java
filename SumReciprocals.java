c4c94d89808fc374c8d55ec36fe5a5271649ce2a
Chih-Chao Chang <cchan113@jhu.edu>
2016-11-02 21:00:02 -0400

    Create SumReciprocals.java

---
+++ SumReciprocals.java
@@ -0,0 +1,11 @@
+
+public class SumReciprocals {
+	public static void main (String[] args)
+	{		
+		double q,num=0.0;	
+		for (q=1;q<=10;q++){
+			num=num+1/q;	
+		}
+		System.out.println("sum is "+num);
+	}
+}
