1835559304177896e86fa6dba0135e15220aa3df
Chih-Chao Chang <cchan113@jhu.edu>
2016-11-02 20:59:06 -0400

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,9 @@
+
+public class Name {
+		public static void main (String[] args)
+		{		
+			System.out.println("+----------+");
+			System.out.println("| cchan113 |");
+			System.out.println("+----------+");
+		}
+}
